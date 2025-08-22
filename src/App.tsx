9776d8c234e626ec97bf4790f3d54d8ed7b9d345
tungtt <thanhtungcis60@gmail.com>
2025-08-22 16:46:34 +0700

    S20-P173. Thực hành Props P1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Footer, Header } from './components/common';
+import { Student } from './features/labs/Student';
 // import Header from './components/common/Header';
 // import Footer from './components/common/Footer';
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Student name="3T" age={16} isHero={false} hobbyList={['eat','code','sleep']} sayHello={()=>console.log("Hello World")}/>
       <Footer/>
     </>
   )
--- src/features/labs/Student.tsx
+++ src/features/labs/Student.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 
-export interface StudentProps {}
+export interface StudentProps {
+    name?: string,
+    age:number,
+    isHero?:boolean,
+    hobbyList: string[],
+    sayHello:()=>void
+}
 
-export function Student (props: StudentProps) {
+export function Student ({name = 'Easy FrontEnd', isHero=false, age=0}: StudentProps) {
   return (
     <div>
-      
+      Hello {name} {isHero ? 'hero' : 'nohero'} {age > 18 ?'adult':'baby'}
     </div>
   );
 }
